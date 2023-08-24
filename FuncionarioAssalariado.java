b504f36aa2591768c372d548c5bb14b1a8a1d52c
Ana Luísa Chagas <114260644+analuisabc@users.noreply.github.com>
2023-08-23 21:36:17 -0300

    Delete FuncionarioAssalariado.java

--- FuncionarioAssalariado.java
+++
@@ -1,9 +0,0 @@
-package AnaLuisaBCGiovanaVBPAbstract;
-
-// Alunas:
-// Ana Luísa de Bastos Chagas (202200491)
-// Giovana Vilas Boas do Prado (202200514)
-public interface FuncionarioAssalariado {
-    public void receberSalario(int nTurmas);
-}
-
