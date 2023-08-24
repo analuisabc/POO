3b1712e21c044440ab9c374fb42b3bff632d9ee2
Ana Luísa Chagas <114260644+analuisabc@users.noreply.github.com>
2023-08-23 21:35:59 -0300

    Delete Estudante.java

--- Estudante.java
+++
@@ -1,8 +0,0 @@
-package AnaLuisaBCGiovanaVBPAbstract;
-
-// Alunas:
-// Ana Luísa de Bastos Chagas (202200491)
-// Giovana Vilas Boas do Prado (202200514)
-public interface Estudante {
-    public void estudar();
-}
