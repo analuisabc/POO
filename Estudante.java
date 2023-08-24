c6da4707f27814180bbb33db5f2f0c0f8b3419b1
Ana Luísa Chagas <114260644+analuisabc@users.noreply.github.com>
2023-08-23 21:29:54 -0300

    Add files via upload

---
+++ Estudante.java
@@ -0,0 +1,8 @@
+package AnaLuisaBCGiovanaVBPAbstract;
+
+// Alunas:
+// Ana Luísa de Bastos Chagas (202200491)
+// Giovana Vilas Boas do Prado (202200514)
+public interface Estudante {
+    public void estudar();
+}
